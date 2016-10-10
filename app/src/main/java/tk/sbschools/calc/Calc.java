c8a67a0ca24e597e48dd9d1a22df4ecf1a81c5c5
2A_Studios <thesniper08512@gmail.com>
2016-10-09 20:51:34 -0400

    Major Revision Commit: V1.4.1 Sine, Cosine, Tangent Parenthesis Dynamic Equation Output Line #Working 4 function calc TODO: Slide In Menu Autocomplete Parenthesis Fractions Graphing Function
    Notes: Converted from Jaxl expression evaluator, to JEval

--- app/src/main/java/tk/sbschools/calc/Calc.java
+++ app/src/main/java/tk/sbschools/calc/Calc.java
@@ -11,22 +11,23 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.apache.commons.jexl3.*;
+import net.sourceforge.jeval.EvaluationConstants;
+import net.sourceforge.jeval.EvaluationException;
+import net.sourceforge.jeval.Evaluator;
 
 import java.util.Locale;
 
 public class Calc extends AppCompatActivity {
-    int dispLen = 11;
+    int dispLen = 110;
     TextView display, prevAws;
     Button clearAll;
-    JexlEngine ExpressionEval = new JexlBuilder().create();
 
+    Evaluator eval = new Evaluator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_calc);
         display = (TextView)findViewById(R.id.Display);
         Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Roboto-Light.ttf");
@@ -46,95 +47,92 @@ public class Calc extends AppCompatActivity {
     }
 
     public void calcPressed(View v){
+        if(display.getText().length() >= 11){
+            display.setTextSize(48);
+            if(display.getText().length() >= 28){
+                display.setTextSize(24);
+                if(display.getText().length() >= 116){
+                    display.setTextSize(18);
+                    if(display.getText().length() >= 140){
+                        Toast.makeText(Calc.this, "Twitter has a 140 character limit... What are you doing?", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(Calc.this, "At this point, possibly consider using a physical calculator.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            //System.err.println(display.length());
+        }else{
+            display.setTextSize(64);
+        }
         switch (v.getId()){
             case (R.id.button_0):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("0");
-                    }else{
-                        display.setText(display.getText() + "0");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("0");
+                }else{
+                    display.setText(display.getText() + "0");
                 }
             }break;
             case (R.id.button_1):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("1");
-                    }else{
-                        display.setText(display.getText() + "1");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("1");
+                }else{
+                    display.setText(display.getText() + "1");
                 }
             }break;
             case (R.id.button_2):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("2");
-                    }else {
-                        display.setText(display.getText() + "2");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("2");
+                }else {
+                    display.setText(display.getText() + "2");
                 }
             }break;
             case (R.id.button_3):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("3");
-                    }else {
-                        display.setText(display.getText() + "3");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("3");
+                }else {
+                    display.setText(display.getText() + "3");
                 }
             }break;
             case (R.id.button_4):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("4");
-                    }else {
-                        display.setText(display.getText() + "4");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("4");
+                }else {
+                    display.setText(display.getText() + "4");
                 }
             }break;
             case (R.id.button_5):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("5");
-                    }else {
-                        display.setText(display.getText() + "5");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("5");
+                }else {
+                    display.setText(display.getText() + "5");
                 }
             }break;
             case (R.id.button_6):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("6");
-                    }else {
-                        display.setText(display.getText() + "6");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("6");
+                }else {
+                    display.setText(display.getText() + "6");
                 }
             }break;
             case (R.id.button_7):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("7");
-                    }else {
-                        display.setText(display.getText() + "7");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("7");
+                }else {
+                    display.setText(display.getText() + "7");
                 }
             }break;
             case (R.id.button_8):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("8");
-                    }else {
-                        display.setText(display.getText() + "8");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("8");
+                }else {
+                    display.setText(display.getText() + "8");
                 }
             }break;
             case (R.id.button_9):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        display.setText("9");
-                    }else {
-                        display.setText(display.getText() + "9");
-                    }
+                if(display.getText().equals("0")){
+                    display.setText("9");
+                }else {
+                    display.setText(display.getText() + "9");
                 }
             }break;
             case (R.id.button_dec):{
@@ -143,69 +141,104 @@ public class Calc extends AppCompatActivity {
                 }
             }break;
             case (R.id.button_add):{
-                if(display.getText().length() < dispLen){
                     if(display.getText().equals("0")){
                         if(prevAws.getText().length() > 0)
                             display.setText(prevAws.getText() + "+");
                     }else {
                         display.setText(display.getText() + "+");
                     }
-                }
             }break;
             case (R.id.button_subtract):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        if(prevAws.getText().length() > 0)
-                            display.setText(prevAws.getText() + "-");
-                        else
-                            display.setText("0-");
-                    }else {
-                        display.setText(display.getText() + "-");
-                    }
+                if(display.getText().equals("0")){
+                    if(prevAws.getText().length() > 0)
+                        display.setText(prevAws.getText() + "-");
+                    else
+                        display.setText("0-");
+                }else {
+                    display.setText(display.getText() + "-");
                 }
             }break;
             case (R.id.button_multiply):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        if(prevAws.getText().length() > 0)
-                            display.setText(prevAws.getText() + "×");
-                        else
-                            display.setText("0×");
-                    }else {
-                        display.setText(display.getText() + "×");
-                    }
+                if(display.getText().equals("0")){
+                    if(prevAws.getText().length() > 0)
+                        display.setText(prevAws.getText() + "×");
+                    else
+                        display.setText("0×");
+                }else {
+                    display.setText(display.getText() + "×");
                 }
             }break;
             case (R.id.button_div):{
-                if(display.getText().length() < dispLen){
-                    if(display.getText().equals("0")){
-                        if(prevAws.getText().length() > 0)
-                            display.setText(prevAws.getText() + "÷");
-                        else
-                            display.setText("0÷");
-                    }else {
-                        display.setText(display.getText() + "÷");
-                    }
+                if(display.getText().equals("0")){
+                    if(prevAws.getText().length() > 0)
+                        display.setText(prevAws.getText() + "÷");
+                    else
+                        display.setText("0÷");
+                }else {
+                    display.setText(display.getText() + "÷");
+                }
+            }break;
+            case (R.id.button_sine):{
+                if(display.getText().equals("0")){
+                    display.setText("sin(");
+                }else {
+                    display.setText(display.getText() + "sin(");
+                }
+            }break;
+            case (R.id.button_cosine):{
+                if(display.getText().equals("0")){
+                    display.setText("cos(");
+                }else {
+                    display.setText(display.getText() + "cos(");
+                }
+            }break;
+            case (R.id.button_tangent):{
+                if(display.getText().equals("0")){
+                    display.setText("tan(");
+                }else {
+                    display.setText(display.getText() + "tan(");
+                }
+            }break;
+            case (R.id.button_left_parenthesis):{
+                if(display.getText().equals("0")){
+                    display.setText("(");
+                }else {
+                    display.setText(display.getText() + "(");
+                }
+            }break;
+            case (R.id.button_right_parenthesis):{
+                if(display.getText().equals("0")){
+                    display.setText(")");
+                }else {
+                    display.setText(display.getText() + ")");
+                }
+            }break;
+            case (R.id.button_power):{
+                if(display.getText().equals("0")){
+                    display.setText("^");
+                }else {
+                    display.setText(display.getText() + "^");
                 }
             }break;
             case (R.id.button_commit):{
                 String equation = display.getText().toString().replaceAll("÷"," / ");
                 equation = equation.replaceAll("×", " * ");
-                equation = equation.replaceAll("[+]", " + ");
-                equation = equation.replaceAll("-", " - ");
-                Number result = 0;
+                equation = equation.replaceAll("sin\\(", " sin( ");
+                equation = equation.replaceAll("cos\\(", " cos( ");
+                equation = equation.replaceAll("tan\\(", " tan( ");
+                equation = equation.replaceAll(" \\.", " 0.");
+                System.err.println(equation);
+                String result = "0";
                 try {
-                    JexlExpression e = ExpressionEval.createExpression(equation);
-                    JexlContext context = new MapContext();
-
-                    result = (Number) e.evaluate(context);
+                    result = eval.evaluate(equation);
                 }catch(Exception e){
-                    Toast.makeText(Calc.this, "Ya dun fucked up: " + e.toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Calc.this, "Error: " + e.toString(), Toast.LENGTH_SHORT).show();
                     display.setText("0");
                     System.err.println(e);
                 }
-                prevAws.setText(result.toString());
+                prevAws.setText(result);
                 display.setText("0");
+                display.setTextSize(64);
             }break;
             case (R.id.button_clear):{
                 if(display.getText().length() > 1)
